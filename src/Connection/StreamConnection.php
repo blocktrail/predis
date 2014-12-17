3ccf6d00fca63cc3cda8de9bd92b69d9308d5492
Ruben de Vries <ruben@rubensayshi.com>
2014-12-17 14:37:46 +0100

    patch for being able to get pubsub to work on hhvm

--- src/Connection/StreamConnection.php
+++ src/Connection/StreamConnection.php
@@ -87,8 +87,11 @@ class StreamConnection extends AbstractConnection
 
         if (isset($parameters->read_write_timeout)) {
             $rwtimeout = (float) $parameters->read_write_timeout;
-            $rwtimeout = $rwtimeout > 0 ? $rwtimeout : -1;
-            $timeoutSeconds  = floor($rwtimeout);
+            if ($rwtimeout == -2.0) {
+                $rwtimeout = 0.0;
+            } else if ($rwtimeout <= 0.0) {
+                $rwtimeout = -1.0;
+            }            $timeoutSeconds  = floor($rwtimeout);
             $timeoutUSeconds = ($rwtimeout - $timeoutSeconds) * 1000000;
             stream_set_timeout($resource, $timeoutSeconds, $timeoutUSeconds);
         }
